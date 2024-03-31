0cb9b640083ec07996df210a772f09d9dfced830
wifikunetworks <163966198+wifikunetworks@users.noreply.github.com>
2024-03-31 19:22:51 +0700

    Update lancar.sh

--- lancar.sh
+++ lancar.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
-log_file="/usr/antibengong/log.txt"
-modem_port="/dev/ttyACM2"
-check_interval=3
-offline_threshold=20 # 20 * 3 detik = 60 detik (1 menit)
-
-restart_modem() {
-    echo "Restarting modem..."
-    echo "at+cfun=1,1" > "$modem_port"
-    sleep 5
+# Fungsi untuk menulis log
+write_log() {
+    echo "$(date +"%A %d %B %Y %T")  Status: $1" >> /etc/antibengong/log.txt
 }
 
-restart_modem_interface() {
-    echo "Restarting modem interface..."
-    ifdown mm && ifup mm
-    sleep 5
-}
+# Inisialisasi jumlah status offline berturut-turut
+offline_count=0
 
-check_internet_connection() {
+# Loop utama
+while true; do
+    # Cek koneksi internet
     if wget -q --spider http://www.gstatic.com/generate_204; then
-        echo "$(date +'%A %d %B %Y %T')  Status: ONLINE" >> "$log_file"
+        # Jika koneksi online
+        write_log "ONLINE"
+        # Reset offline count
+        offline_count=0
     else
-        echo "$(date +'%A %d %B %Y %T')  Status: OFFLINE" >> "$log_file"
-    fi
-}
-
-main() {
-    while true; do
-        check_internet_connection
-        sleep $check_interval
-
-        # Check if offline for more than 1 minute
-        offline_count=$(grep -c "Status: OFFLINE" "$log_file")
-        if [[ $offline_count -ge $offline_threshold ]]; then
-            restart_modem
-            restart_modem_interface
-            echo "Internet connection restored after restarting modem and interface."
+        # Jika koneksi offline
+        write_log "OFFLINE"
+        ((offline_count++))
+        # Jika offline lebih dari 20 kali berturut-turut, restart modem dan interface
+        if [ $offline_count -ge 20 ]; then
+            write_log "Restart Modem & Interface"
+            # Restart modem
+            echo "at+cfun=1,1" > /dev/ttyACM2
+            # Restart interface modem
+            ifdown mm && ifup mm
+            # Reset offline count
+            offline_count=0
         fi
-    done
-}
-
-main
+    fi
+    # Tunggu 3 detik
+    sleep 3
+done
