033c31629dfb275844e280b43ad1c913c3e1b258
wifikunetworks <163966198+wifikunetworks@users.noreply.github.com>
2024-04-06 09:41:37 +0700

    Update online.sh

--- online.sh
+++ online.sh
@@ -11,13 +11,13 @@ while true; do
     timestamp=$(date +"%A %d %B %Y %T")
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $url_to_check)
     if [ $http_code -eq 204 ]; then
-        echo "$timestamp  Status: ONLINE" >> $log_file
+        echo "$timestamp Status: ONLINE" >> $log_file
         offline_count=0
     else
-        echo "$timestamp  Status: OFFLINE" >> $log_file
+        echo "$timestamp Status: OFFLINE" >> $log_file
         offline_count=$((offline_count + 1))
         if [ $offline_count -eq 5 ]; then
-            echo "$timestamp  Status: Restart Modem & Interface" >> $log_file
+            echo "$timestamp Status: Restart Modem & Interface" >> $log_file
             eval $restart_modem_command
             sleep 10 # Tambahkan penundaan setelah restart modem
             eval $restart_interface_command
